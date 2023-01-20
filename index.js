d1c5952fa65a04606c620d0179f2effbef25e63c
sofiane <saissaoui@figarocms.fr>
2023-01-21 00:37:50 +0100

    change to node project

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+
+var app = express();
+const path = require('path');
+
+app.use('/img', express.static(path.join(__dirname, 'img')))
+app.use('/lib', express.static(path.join(__dirname, 'lib')))
+app.use('/dist', express.static(path.join(__dirname, 'dist')))
+
+app.get('/', function (req, res) {
+
+res.sendFile(path.join(__dirname+'/index.html'));
+    
+});
+
+app.get('/terrain2', function (req, res) {
+
+res.sendFile(path.join(__dirname+'/terrain2.html'));
+    
+});
+
+app.get('/terrain3', function (req, res) {
+
+res.sendFile(path.join(__dirname+'/terrain3.html'));
+    
+});
+
+app.listen(3000, function () {
+
+console.log('running');
+
+});
